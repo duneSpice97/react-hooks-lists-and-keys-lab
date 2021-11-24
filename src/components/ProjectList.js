ba09f23d242eb909c2c29991f7c879a8f03f60b3
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-11-24 11:55:10 -0700

    commit tree ff3094af773882bc4853535eaf6cc565e2581520

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,26 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const projectsRender = projects.map((project) => {
+    return (
+      <ProjectItem
+        key={Math.random()}
+        name={project.name}
+        about={project.about}
+        technologies={project.technologies}
+      />
+    );
+  });
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectsRender}</div>
     </div>
   );
 }
 
 export default ProjectList;
+
+   {/*const info = projects.map(({ name, about }) => {
+   <ProjectItem key={id} name={name} about={about} />;*/}
